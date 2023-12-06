d814e5fad5822a27aa7e2542774485bd4c36005e
Carl Saqui <carlwilsonlaborsaqui@gmail.com>
2023-12-07 00:21:50 +0800

    fixed error in WelcomeControllerTest case

--- test/controllers/welcome_controller_test.rb
+++ test/controllers/welcome_controller_test.rb
@@ -2,7 +2,8 @@ require "test_helper"
 
 class WelcomeControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get welcome_index_url
+    # Instead of using welcome_index_url, you can use the root_url
+    get root_url
     assert_response :success
   end
 end
